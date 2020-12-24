a884f19915ebe20551e8a160692e451162374905
KOU256 <kou256.8bit+github@gmail.com>
2020-12-25 01:08:04 +0900

    Frequencer.java: 2分探索による検索を実装
    2分探索でStartIndexとEndIndexを検索するようにした。

--- s4/B203337/Frequencer.java
+++ s4/B203337/Frequencer.java
@@ -207,7 +207,7 @@ public class Frequencer implements FrequencerInterface{
         byte[] suffix_i = Arrays.copyOfRange(mySpace, i, mySpace.length);
         byte[] target_j_k = Arrays.copyOfRange(myTarget, j, k);
 
-        for (int l = 0; l < suffix_i.length && l < target_j_k.length; l++) {
+        for (int l = 0; l < Math.min(suffix_i.length, target_j_k.length); l++) {
             if (suffix_i[l] > target_j_k[l]) {
                 return 1;
             }
@@ -255,13 +255,22 @@ public class Frequencer implements FrequencerInterface{
         //
         // ここにコードを記述せよ。
         //
-        for (int i = 0; i < suffixArray.length; i++) {
-            if (targetCompare(suffixArray[i], start, end) == 0) {
-                return i;
+        int min = 0, max = suffixArray.length, index = max;
+        while (min < max) {
+            int mid = (min + max) / 2;
+            int result = targetCompare(suffixArray[mid], start, end);
+            if (result == 1) {
+                max = mid - 1;
+            }
+            else if (result == -1) {
+                min = mid + 1;
+            }
+            else if (result == 0) {
+                index = mid;
+                max = mid;
             }
         }
-        
-        return suffixArray.length; //このコードは変更しなければならない。
+        return index;
     }
 
     private int subByteEndIndex(int start, int end) {
@@ -293,13 +302,22 @@ public class Frequencer implements FrequencerInterface{
         //
         //　ここにコードを記述せよ
         //
-        for (int i = 0; i < suffixArray.length; i++) {
-            if (targetCompare(suffixArray[i], start, end) == 1) {
-                return i;
+        int min = 0, max = suffixArray.length, index = max;
+        while (min < max) {
+            int mid = (min + max) / 2;
+            int result = targetCompare(suffixArray[mid], start, end);
+            if (result == 1) {
+                index = mid;
+                max = mid;
+            }
+            else if (result == -1) {
+                min = mid + 1;
+            }
+            else if (result == 0) {
+                min = mid + 1;
             }
         }
-
-        return suffixArray.length; // この行は変更しなければならない、
+        return index;
     }
 
 
